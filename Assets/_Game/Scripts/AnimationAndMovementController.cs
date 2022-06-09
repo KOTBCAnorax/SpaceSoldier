a90c37ff7c7a9756b935ea6e520bcbb8460d1bf8
KOTBCAnorax <47pohih@gmail.com>
2022-06-09 17:05:30 +0300

    Refactoring

--- Assets/_Game/Scripts/AnimationAndMovementController.cs
+++ Assets/_Game/Scripts/AnimationAndMovementController.cs
@@ -7,7 +7,8 @@ public class AnimationAndMovementController : MonoBehaviour
     [SerializeField] private float _playerSpeed = 2.0f;
     [SerializeField] private Animator _animator;
     [SerializeField] private CharacterController _controller;
-    [SerializeField] private float _deadzone = 0.8f;
+    [SerializeField] private float _deadzoneMax = 0.8f;
+    [SerializeField] private float _deadzoneMin = 0.05f;
     [SerializeField] private Transform _cam;
 
     private PlayerInput _playerInput;
@@ -41,7 +42,7 @@ public class AnimationAndMovementController : MonoBehaviour
         Vector2 inputVector = _playerInput.CharacterControls.Run.ReadValue<Vector2>();
         RotateInInputDirection(inputVector);
 
-        if (inputVector.magnitude > _deadzone)
+        if (inputVector.magnitude > _deadzoneMax)
         {
             _animator.SetBool(_isRunningForwardHash, true);
             _controller.Move(transform.forward * _playerSpeed * Time.deltaTime);
@@ -54,6 +55,11 @@ public class AnimationAndMovementController : MonoBehaviour
 
     private void RotateInInputDirection(Vector2 inputVector)
     {
+        if (inputVector.magnitude < _deadzoneMin)
+        {
+            return;
+        }
+
         Vector3 direction = new Vector3(inputVector.x, 0f, inputVector.y);
         float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _cam.eulerAngles.y;
         transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
